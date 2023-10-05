8660853dab1a42bd7cc03725d695ed408337449a
schwinger80 <63354678+schwinger80@users.noreply.github.com>
2023-10-05 10:56:24 +0200

    Update gpfs.go
    complete restructuring

--- plugins/inputs/gpfs/gpfs.go
+++ plugins/inputs/gpfs/gpfs.go
@@ -1,103 +1,58 @@
+// go:generate ../../../tools/readme_config_includer/generator
 package gpfs
 
 import (
-        "bufio"
-        "os/exec"
-        "strings"
-        "strconv"
-        "github.com/influxdata/telegraf"
-        "github.com/influxdata/telegraf/plugins/inputs"
+	"bufio"
+	"os"
+	"strings"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
-type CommandRunner interface {
-        RunCommand(command string) (*bufio.Scanner, error)
+//go:embed sample.conf
+var sampleConfig string
+
+type GPFS struct {
+	PipePath string         `toml:"pipe_path"`
+	Log      telegraf.Logger `toml:"-"`
 }
 
-type RealCommandRunner struct{}
-
-func (r *RealCommandRunner) RunCommand(command string) (*bufio.Scanner, error) {
-        cmd := exec.Command("bash", "-c", command)
-        stdout, err := cmd.StdoutPipe()
-        if err != nil {
-                return nil, err
-        }
-        if err := cmd.Start(); err != nil {
-                return nil, err
-        }
-        return bufio.NewScanner(stdout), nil
+func (*GPFS) SampleConfig() string {
+	return sampleConfig
 }
 
-type GPFSStats struct {
-        Command string `toml:"command"`
-        Runner  CommandRunner
+func (g *GPFS) Init() error {
+	// Hier können Sie eine Überprüfung durchführen, ob die named pipe existiert oder andere Initialisierungen.
+	return nil
 }
 
-func (g *GPFSStats) Description() string {
-        return "Gather GPFS Performance Statistics using mmpmon"
-}
+func (g *GPFS) Gather(acc telegraf.Accumulator) error {
+	pipe, err := os.Open(g.PipePath)
+	if err != nil {
+		return err
+	}
+	defer pipe.Close()
 
-func (g *GPFSStats) SampleConfig() string {
-        return `command = "mmpmon -p -c 'fs_io_s'" `
-}
+	reader := bufio.NewReader(pipe)
+	line, _, err := reader.ReadLine()
+	if err != nil {
+		return err
+	}
 
-func (g *GPFSStats) Gather(acc telegraf.Accumulator) error {
-        if g.Runner == nil {
-                g.Runner = &RealCommandRunner{}
-        }
-        scanner, err := g.Runner.RunCommand(g.Command)
-        if err != nil {
-                return err
-        }
+	// Hier ist eine einfache Annahme: Der String ist "key=value"
+	parts := strings.Split(string(line), "=")
+	if len(parts) != 2 {
+		return fmt.Errorf("Unexpected format: %s", line)
+	}
 
-        var priorT, priorTu, priorBr, priorBw float64
-        count := 0
+	// Speichert den Wert im Telegraf-Akkumulator
+	acc.AddFields("gpfs", map[string]interface{}{parts[0]: parts[1]}, nil)
 
-        for scanner.Scan() {
-                line := scanner.Text()
-                fields := strings.Fields(line)
-                if len(fields) < 22 {
-                        continue // Ungültige Zeile, überspringen
-                }
-
-                t, _ := strconv.ParseFloat(fields[8], 64)
-                tu, _ := strconv.ParseFloat(fields[10], 64)
-                br, _ := strconv.ParseFloat(fields[18], 64)
-                bw, _ := strconv.ParseFloat(fields[20], 64)
-
-                if count > 0 {
-                        deltaT := t - priorT
-                        deltaTu := tu - priorTu
-                        deltaBr := br - priorBr
-                        deltaBw := bw - priorBw
-                        dt := deltaT + (deltaTu / 1000000.0)
-                        if dt > 0 {
-                                rrate := (deltaBr / dt) / 1000000.0
-                                wrate := (deltaBw / dt) / 1000000.0
-
-                                // Füge die Messwerte zum Akkumulator hinzu
-                                fields := map[string]interface{}{
-                                        "read_rate":  rrate,
-                                        "write_rate": wrate,
-                                }
-                                acc.AddFields("gpfs_io", fields, nil)
-                        }
-                }
-
-                priorT = t
-                priorTu = tu
-                priorBr = br
-                priorBw = bw
-                count++
-        }
-
-        if err := scanner.Err(); err != nil {
-                return err
-        }
-
-        return nil
+	return nil
 }
 
 func init() {
-    inputs.Add("gpfs", func() telegraf.Input { return &Gpfs{} })
+	inputs.Add("gpfs", func() telegraf.Input { return &Gpfs{} })
 }
 
