e2c1029fd46c398aa42294b358a0c69bba8c4e18
schwinger80 <63354678+schwinger80@users.noreply.github.com>
2023-10-22 16:23:38 +0200

    Update gpfs_test.go
    go v 1.20 version

--- plugins/inputs/gpfs/gpfs_test.go
+++ plugins/inputs/gpfs/gpfs_test.go
@@ -2,7 +2,9 @@ package gpfs_io
 
 import (
 	"testing"
-	"github.com/influxdata/telegraf/testutil"
+	"log"
+	"strings"
+	"strconv"
 )
 
 func TestGPFSIO_Gather(t *testing.T) {
@@ -11,45 +13,72 @@ func TestGPFSIO_Gather(t *testing.T) {
 		PipePath: "/path/to/your/namedpipe",
 	}
 
-	// Erstellen Sie einen Akkumulator für Testzwecke
-	acc := testutil.Accumulator{}
+	// Erstellen Sie einen Akkumulator für Testzwecke (eine einfache Map)
+	acc := make(map[string]interface{})
 
 	// Rufen Sie die Gather-Methode Ihres Plugins auf
-	err := g.Gather(&acc)
+	err := g.Gather(acc)
 	if err != nil {
 		t.Errorf("Fehler beim Ausführen von Gather: %v", err)
 	}
 
-	// Erstellen Sie eine Slice von telegraf.Metric für erwartete Metriken
-	expectedMetrics := []telegraf.Metric{
-		// Hier können Sie erwartete Metriken hinzufügen, die Sie erwarten.
-		// Beispiel: testutil.MustMetric(
-		//     "gpfs_io_mmpmon",
-		//     map[string]string{
-		//         "ip_address":   "10.156.153.84",
-		//         "hostname":     "hpdar03c04s08",
-		//         "cluster_name": "LRZ_DSS03.dss.lrz.de",
-		//         // Weitere Tags hier
-		//     },
-		//     map[string]interface{}{
-		//         "status_code":    0,
-		//         "timestamp":      int64(1697982519),
-		//         "microseconds":   int(24222),
-		//         "disk_count":     int(84),
-		//         "bytes_read":     uint64(22794336358085),
-		//         "bytes_written":  uint64(0),
-		//         "open_calls":     int(127818),
-		//         "close_calls":    int(127817),
-		//         "read_calls":     int(34356262),
-		//         "write_calls":    int(0),
-		//         "readdir_calls":  int(46906),
-		//         "inode_updates":  int(76710),
-		//         // Weitere Felder hier
-		//     },
-		//     int64(1697982519),
-		// )
+	// Überprüfen Sie, ob die gesammelten Daten den erwarteten Werten entsprechen
+	expectedMetrics := map[string]interface{}{
+		// Hier können Sie erwartete Felder und Werte hinzufügen, die Sie erwarten.
+		// Beispiel: "status_code": 0,
+		// "timestamp": int64(1697982519),
+		// "microseconds": int(24222),
+		// "disk_count": int(84),
+		// "bytes_read": uint64(22794336358085),
+		// "bytes_written": uint64(0),
+		// "open_calls": int(127818),
+		// "close_calls": int(127817),
+		// "read_calls": int(34356262),
+		// "write_calls": int(0),
+		// "readdir_calls": int(46906),
+		// "inode_updates": int(76710),
+		// Weitere Felder hier
 	}
 
-	// Überprüfen Sie, ob die tatsächlichen Metriken den erwarteten Metriken entsprechen
-	testutil.RequireMetricsEqual(t, expectedMetrics, acc.GetTelegrafMetrics())
+	// Vergleichen Sie die gesammelten Daten mit den erwarteten Werten
+	for field, expectedValue := range expectedMetrics {
+		actualValue, found := acc[field]
+		if !found {
+			t.Errorf("Feld %s nicht in den gesammelten Daten gefunden", field)
+		}
+
+		// Konvertieren Sie den erwarteten Wert in den richtigen Typ und vergleichen Sie ihn
+		switch expectedValue.(type) {
+		case int:
+			actualInt, err := strconv.Atoi(actualValue.(string))
+			if err != nil {
+				t.Errorf("Feld %s sollte einen int-Wert haben, aber der Wert konnte nicht konvertiert werden", field)
+			}
+			if actualInt != expectedValue.(int) {
+				t.Errorf("Feld %s hat einen unerwarteten Wert. Erwartet: %d, erhalten: %d", field, expectedValue.(int), actualInt)
+			}
+		case int64:
+			actualInt64, err := strconv.ParseInt(actualValue.(string), 10, 64)
+			if err != nil {
+				t.Errorf("Feld %s sollte einen int64-Wert haben, aber der Wert konnte nicht konvertiert werden", field)
+			}
+			if actualInt64 != expectedValue.(int64) {
+				t.Errorf("Feld %s hat einen unerwarteten Wert. Erwartet: %d, erhalten: %d", field, expectedValue.(int64), actualInt64)
+			}
+		case uint64:
+			actualUint64, err := strconv.ParseUint(actualValue.(string), 10, 64)
+			if err != nil {
+				t.Errorf("Feld %s sollte einen uint64-Wert haben, aber der Wert konnte nicht konvertiert werden", field)
+			}
+			if actualUint64 != expectedValue.(uint64) {
+				t.Errorf("Feld %s hat einen unerwarteten Wert. Erwartet: %d, erhalten: %d", field, expectedValue.(uint64), actualUint64)
+			}
+		case string:
+			if actualValue.(string) != expectedValue.(string) {
+				t.Errorf("Feld %s hat einen unerwarteten Wert. Erwartet: %s, erhalten: %s", field, expectedValue.(string), actualValue.(string))
+			}
+		default:
+			t.Errorf("Ungültiger Datentyp für das Feld %s", field)
+		}
+	}
 }
