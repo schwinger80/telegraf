0072b1a7626f692fb4a574b2562e47a2c2922bc5
schwinger80 <63354678+schwinger80@users.noreply.github.com>
2023-10-23 10:21:25 +0200

    Update gpfs_test.go

--- plugins/inputs/gpfs/gpfs_test.go
+++ plugins/inputs/gpfs/gpfs_test.go
@@ -3,6 +3,8 @@ package gpfs_io
 import (
 	"testing"
 	"strconv"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/testutil"
 )
 
 func TestGPFSIO_Gather(t *testing.T) {
@@ -11,45 +13,43 @@ func TestGPFSIO_Gather(t *testing.T) {
 		PipePath: "/path/to/your/namedpipe",
 	}
 
-	// Erstellen Sie eine einfache Map, um die gesammelten Daten zu speichern
-	collectedData := map[string]interface{}{}
+	// Erstellen Sie eine leere Instanz von telegraf.Accumulator
+	acc := &testutil.Accumulator{}
 
-	// Rufen Sie die Gather-Methode Ihres Plugins auf und übergeben die Map
-	err := g.Gather(&collectedData)
+	// Rufen Sie die Gather-Methode Ihres Plugins auf und übergeben die Instanz von telegraf.Accumulator
+	err := g.Gather(acc)
 	if err != nil {
 		t.Errorf("Fehler beim Ausführen von Gather: %v", err)
 	}
 
 	// Überprüfen Sie, ob die gesammelten Daten den erwarteten Werten entsprechen
-	expectedMetrics := map[string]interface{}{
-		"_n_":           "10.156.153.84",
-		"_nn_":          "hpdar03c04s08",
-		"_rc_":          0,
-		"_t_":           int64(1697982519),
-		"_tu_":          24222,
-		"_cl_":          "LRZ_DSS03.dss.lrz.de",
-		"_fs_":          "dsstbyfs01",
-		"_d_":           84,
-		"_br_":          uint64(22794336358085),
-		"_bw_":          uint64(0),
-		"_oc_":          127818,
-		"_cc_":          127817,
-		"_rdc_":         34356262,
-		"_wc_":          0,
-		"_dir_":         46906,
-		"_iu_":          76710,
+	expectedMetrics := []testutil.Metric{
+		testutil.MustMetric(
+			"gpfs_io_mmpmon",
+			map[string]string{
+				"_n_":  "10.156.153.84",
+				"_nn_": "hpdar03c04s08",
+				"_cl_": "LRZ_DSS03.dss.lrz.de",
+				"_fs_": "dsstbyfs01",
+			},
+			map[string]interface{}{
+				"_rc_":           0,
+				"_t_":            int64(1697982519),
+				"_tu_":           24222,
+				"_d_":            84,
+				"_br_":           uint64(22794336358085),
+				"_bw_":           uint64(0),
+				"_oc_":           127818,
+				"_cc_":           127817,
+				"_rdc_":          34356262,
+				"_wc_":           0,
+				"_dir_":          46906,
+				"_iu_":           76710,
+			},
+		),
+		// Weitere erwartete Metriken hier hinzufügen, falls erforderlich
 	}
 
-	// Vergleichen Sie die gesammelten Daten mit den erwarteten Werten
-	for field, expectedValue := range expectedMetrics {
-		actualValue, found := collectedData[field]
-		if !found {
-			t.Errorf("Feld %s nicht in den gesammelten Daten gefunden", field)
-		}
-
-		// Vergleichen Sie die erwarteten und tatsächlichen Werte
-		if actualValue != expectedValue {
-			t.Errorf("Feld %s hat einen unerwarteten Wert. Erwartet: %v, erhalten: %v", field, expectedValue, actualValue)
-		}
-	}
+	// Vergleichen Sie die gesammelten Daten mit den erwarteten Metriken
+	testutil.RequireMetricsEqual(t, expectedMetrics, acc.GetTelegrafMetrics())
 }
